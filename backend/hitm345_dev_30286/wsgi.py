9469050bde4fd6c46dc5535217104a9192cb0955
Crowdbotics <engineering@crowdbotics.com>
2022-08-25 11:16:26 +0000

    Update models

--- backend/hitm345_dev_30286/wsgi.py
+++ backend/hitm345_dev_30286/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hitm345_dev_30286.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hitm345_dev_30286.settings")
 
 application = get_wsgi_application()
